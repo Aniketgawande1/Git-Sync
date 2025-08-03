acbb382e6d3bbcf00c90c9522390fa9571b42183
Aniketgawande1 <aniketgaw2005@gamil.com>
2025-08-03 13:15:10 +0530

    final commit

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='git-sync',
-    version='1.0.0',
+    name="git-sync",
+    version="1.0.0",
     py_modules=['cli'],
     packages=find_packages(),
     install_requires=[
-        'typer[all]',
-        'rich',
-        'gitpython',
-        'psutil',
+        "typer",
+        "rich",
+        "gitpython",
+        "psutil"
     ],
     entry_points={
-        'console_scripts': [
-            'git-sync = cli:app',
-        ],
+        "console_scripts": [
+            "git-sync=cli:app"
+        ]
     },
 )
